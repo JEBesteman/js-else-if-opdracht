140e8b7cb33f063f697ebc2ab7c5591ab89356bf
JEBesteman <86465468+JEBesteman@users.noreply.github.com>
2021-07-29 14:29:27 +0200

    Add files via upload

---
+++ js-if-else/script.js
@@ -0,0 +1,31 @@
+//opdracht 1 kroegbaas
+const age = 30;
+
+if (age >= 18)
+{
+    console.log("Persoon mag naar binnen!");
+}
+ else {
+
+    console.log("Persoon is te jong en wordt vriendelijk verzocht buiten te blijven");
+}
+
+//opdracht 2 ladiesnight
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Welcome to ladiesnight!");
+} else {
+    console.log("Sorry, tonight ladies only!");
+}
+
+//opdracht 3 bob
+
+const driverStatus = "bob";
+
+if (driverStatus === "bob") {
+    console.log("Je bent de bob, dus je mag rijden!");
+} else {
+    console.log("Helaas geen bob, dus je mag absoluut niet rijden!!!");
+}
